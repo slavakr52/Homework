e774966279af7f6433b394416f84a74f289609b3
slavakr52 <slavakr52@mail.ru>
2023-03-14 21:27:53 +0300

    update

--- Homework/Homework020/Task1.java
+++ Homework/Homework020/Task1.java
@@ -16,33 +16,50 @@ public class Task1 {
     public static void main(String[] args) {
 
         LinkedList<String> userList = new LinkedList<>();
+        Scanner input = new Scanner(System.in);
         boolean flag = true;
 
         while (flag == true) {
-        
-            String userInput = userInput();
+
+            String userInput = userInput(input);
 
             switch (userInput) {
                 case "print":
                     System.out.println("case");
-                    break;
+                    flag = false;
 
                 default:
-                System.out.println("def");
-                break;
+                    System.out.println("def");
+                    break;
             }
         }
+        input.close();
     }
 
-    private static String userInput() {
+    private static String userInput(Scanner input) {
 
-        Scanner input = new Scanner(System.in);
+        String text;
         System.out.println("\nYou may add a line or enter action"
                 + "\nAvailable actions:\nprint\nrevert\nexit"
                 + "\nEnter text or action: ");
-        String text = input.nextLine();
-        input.close();
+        if (input.hasNextLine()) {
+            text = input.nextLine();
+        } else {
+            text = "error";
+        }
+        // 
 
         return text.toLowerCase();
     }
 }
+
+/*
+ * public String scan(Scanner s) {
+ * String input;
+ * if (s.hasNextLine()) {
+ * input = s.nextLine();
+ * } else {
+ * input = "ERROR";
+ * }
+ * return input.toLowerCase();
+ */
