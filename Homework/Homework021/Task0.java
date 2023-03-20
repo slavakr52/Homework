a98af998f00c665340b982fc0c5bcbb121807c34
slavakr52 <slavakr52@mail.ru>
2023-03-20 16:38:34 +0300

    start

---
+++ Homework/Homework021/Task0.java
@@ -0,0 +1,19 @@
+package Homework.Homework021;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Task0
+ */
+public class Task0 {
+
+    public static void main(String[] args) {
+        
+        HashMap <String, ArrayList<String>> map = new HashMap<>();
+       
+     
+        
+        System.out.println(map);
+    }
+}
